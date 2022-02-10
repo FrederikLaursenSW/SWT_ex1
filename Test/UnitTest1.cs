700fad0ab8aa7e6e10aa428ccb56109b647af50f
NicoNico1995 <78845976+NicoNico1995@users.noreply.github.com>
2022-02-10 12:32:05 +0100

    substract test done

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -45,5 +45,19 @@ namespace test
 
         }
 
+        [TestCase(10, 5, 5)]
+        [TestCase(7, 17, -10)]
+        [TestCase(-4, -5, 1)]
+
+        public void substract_oneNumberFromAnother_returnSum(int a, int b, int c)
+        {
+            //act
+            double res = testCalc.Subtract(a,b);
+
+            //Assert
+            Assert.That(testCalc.Accumulator, Is.EqualTo(c));
+
+        }
+
     }
 }

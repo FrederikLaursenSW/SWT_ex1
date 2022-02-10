31beac49ac79265ecaf9c22bccff83f34270af7a
NicoNico1995 <78845976+NicoNico1995@users.noreply.github.com>
2022-02-10 12:27:04 +0100

    add tests lavet.

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -16,27 +16,14 @@ namespace test
 
         [TestCase(13,22,35)]
         [TestCase(2, 3, 5)]
-        [TestCase(7, 11, 18)]
-        public void positiveNumbers(int a, int b, int c)
-        {
-
-            //act
-            double res = testCalc.Add(a, b);
-
-
-            //Assert
-            Assert.That(res, Is.EqualTo(c));
-
-        }
-
-        [TestCase(-3, -5, -8)]
         [TestCase(-10, -5, -15)]
         [TestCase(-7, -10, -17)]
-
-        public void negativeNumbers(int a, int b, int c)
+        public void add_twoNumbers_returnSum(int a, int b, int c)
         {
+
             //act
             double res = testCalc.Add(a, b);
+
 
             //Assert
             Assert.That(res, Is.EqualTo(c));
@@ -46,7 +33,7 @@ namespace test
         [TestCase(5, 7, 12)]
         [TestCase(12, 10, 22)]
         [TestCase(-5, 10, 5)]
-        public void addToResult(int a, int b, int c)
+        public void add_oneNumber_returnSum(int a, int b, int c)
         {
             //act
             double res1 = testCalc.Add(a);
